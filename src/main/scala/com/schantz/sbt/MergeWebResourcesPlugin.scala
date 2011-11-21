1fc36c2b521a6db981c79cb4c122e740b40f0702
Lars Tackmann <ltackmann@gmail.com>
2011-11-21 19:48:14 +0100

    initial attempt at recursing classpath for duplicate jars

--- src/main/scala/com/schantz/sbt/MergeWebResourcesPlugin.scala
+++ src/main/scala/com/schantz/sbt/MergeWebResourcesPlugin.scala
@@ -14,8 +14,8 @@ object MergeWebResourcesPlugin extends Plugin {
   def webSettings = {
     // custom tasks
     warResourceDirectoriesTask ++
-    // defaults
-    inConfig(Compile)(Seq(warExcludedJars := Nil, warExcludedMetaInfResources := Nil)) ++
+      // defaults
+      inConfig(Compile)(Seq(warExcludedJars := Nil, warExcludedMetaInfResources := Nil)) ++
       // configure web app
       com.github.siasia.WarPlugin.warSettings ++
       // TODO make this configurable
@@ -28,14 +28,36 @@ object MergeWebResourcesPlugin extends Plugin {
   }
 
   private def warPrepareTask: Initialize[Task[Seq[(File, String)]]] = (warPrepare, target, excludeFilter,
-    streams, warExcludedJars, warExcludedMetaInfResources, warResourceDirectories) map {
-      (pw, target, filter, out, excludedJars, excludedMetainfResources, webResources) =>
+    streams, warExcludedJars, warExcludedMetaInfResources, warResourceDirectories, unmanagedClasspath) map {
+      (warPrep, target, filter, out, excludedJars, excludedMetainfResources, webResources, fullClasspath) =>
         {
           val warPath = target / "webapp"
           val warLibPath = warPath / "WEB-INF/lib"
-          // remove unwanted jar's
+
+          // remove excluded jar's
           out.log.info("Excluding jars from war: " + excludedJars)
           excludedJars.foreach(jar => IO.delete(warLibPath / jar))
+
+          // remove duplicate jar's
+          var versionizeJar: (File => (String, String, File)) = jar => {
+            val jarRegex = """(.*?)(?:-(?=\d)(.*))?.jar""".r
+            val jarRegex(name, version) = jar.getName
+            (name, version, jar)
+          }
+          var allJars = fullClasspath.flatMap { jar => if (jar.data.isFile()) Seq(versionizeJar(jar.data)) else Seq() }
+          var warJars = warLibPath.listFiles().filter(_.isFile()).map { jar => versionizeJar(jar) }
+          allJars.foreach { jarEntry =>
+            var found = (warLibPath / jarEntry._3.getName()).isFile()
+            if (found) {
+              // if jar is found in war then check if other versions exists and delete them
+              warJars.foreach { warEntry =>
+                if (jarEntry._1 == warEntry._1 && jarEntry._2 != warEntry._2) {
+                  out.log.warn("Excluding duplicate jar from war: " + warEntry._3.getAbsolutePath())
+                  IO.delete(warEntry._3)
+                }
+              }
+            }
+          }
 
           // remove unwanted meta-inf content
           out.log.info("Excluding content from meta-inf: " + excludedMetainfResources)
